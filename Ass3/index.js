75671fe28c7a4d6a6f415fe099bf07edcb1547c2
Hamza-Mujahid <Ahsan.Ahmed.026@gmail.com>
2022-11-17 11:21:07 +0500

    Add Ass03

---
+++ Ass3/index.js
@@ -0,0 +1,18 @@
+// document.querySelector("#pro").addEventListener('mouseover', () => {
+//     document.getElementById('pro').style.backgroundColor = "green";
+// })
+
+// document.getElementById("pro").style.backgroundColor = "green";
+
+
+// document.querySelector('#menu').addEventListener('click', () => {
+//     document.querySelector('nav ul').classList.toggle('show')
+// })
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
