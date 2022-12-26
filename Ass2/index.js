ddea6e562bcbe5c4704d5bf4dbc962174ea14945
Ahsan Ahmed <66218657+Ahsan-Ah-med@users.noreply.github.com>
2022-12-26 16:24:54 +0500

    Update Faq + - sign toggle

--- Ass2/index.js
+++ Ass2/index.js
@@ -1,3 +1,17 @@
 document.querySelector('#menu').addEventListener('click', ()=>{
     document.querySelector('nav ul').classList.toggle('show')
 })
+
+// For more then one 
+document.querySelectorAll('.list_item_acc').forEach((s, i) =>{
+      s.addEventListener('click', ()=>{
+        document.querySelectorAll('.faq-logo-plus')[i].classList.toggle('show')
+  })
+    
+// Jugar Code 
+    
+document.querySelectorAll('.list_item_acc').forEach((s, i) =>{
+    s.addEventListener('click', ()=>{
+      s.querySelector('.faq-logo-plus').classList.toggle('show')
+})
+  })
